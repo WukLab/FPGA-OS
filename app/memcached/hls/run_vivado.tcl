267ebea3ee863ac796a2068ed0abcafdb3d4ea3e
Yizhou Shan <ys@purdue.edu>
2019-03-27 17:10:32 -0400

    app/mcd: no need to run synthesis before IP packaging

--- app/memcached/hls/run_vivado.tcl
+++ app/memcached/hls/run_vivado.tcl
@@ -988,8 +988,8 @@ set_property STEPS.POST_ROUTE_PHYS_OPT_DESIGN.ARGS.DIRECTIVE AggressiveExplore [
 ##################################################################
 # run synthesis 
 ##################################################################
-launch_runs synth_1 -jobs 12
-wait_on_run synth_1
+#launch_runs synth_1 -jobs 12
+#wait_on_run synth_1
 
 #launch_runs impl_1 -to_step write_bitstream -jobs 8
 #wait_on_run impl_1
