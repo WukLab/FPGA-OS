bc8eba7abb290229cb8b649ecf9cf4548fba1d01
hythzz <24907190+hythzz@users.noreply.github.com>
2019-03-26 23:55:02 -0400

    [Memcached] hook memcached with buddy, but currently doesn't work

---
+++ app/memcached/hls/run_hls.memcachedBuddy_vcu108.tcl
@@ -0,0 +1,56 @@
+# Copyright (c) 2019, Wuklab, Purdue University.
+#
+# Template script for generating a Vivado HLS project.
+# 1) generated_hls_project/ will be created.
+#    This is listed in top-level .gitignore.
+#    DO NOT CHANGE THIS.
+# 2) solution 1 is used.
+# 3) simulation, synthesis, and IP export will be executed.
+#
+# To customize:
+# 1) Change part and clock
+# 2) Change added file name
+# 3) Change top-level function name
+# 4) Change exported IP parameters
+
+# Create a project
+open_project	-reset generated_hls_project
+
+add_files sources/hashTable/cc.cpp                      -cflags "-I../../../include"
+add_files sources/hashTable/compare.cpp                 -cflags "-I../../../include"
+add_files sources/hashTable/hash.cpp                    -cflags "-I../../../include"
+add_files sources/hashTable/hashTableWithBuddy.cpp      -cflags "-I../../../include"
+add_files sources/hashTable/memRead.cpp                 -cflags "-I../../../include"
+add_files sources/hashTable/memWriteWithBuddy.cpp       -cflags "-I../../../include"
+add_files sources/requestParser/requestParser.cpp       -cflags "-I../../../include"
+add_files sources/responseFormatter/binResponse.cpp     -cflags "-I../../../include"
+add_files sources/valueStore/valueStore.cpp             -cflags "-I../../../include"
+add_files sources/globals.cpp                           -cflags "-I../../../include"
+add_files sources/memcachedBuddy.cpp                    -cflags "-I../../../include"
+
+add_files -tb ../../../alloc/buddy/buddy.cpp            -cflags "-I../../../include"
+add_files -tb ../../../alloc/buddy/core.cpp             -cflags "-I../../../include"
+add_files -tb sources/memcachedBuddy_tb.cpp          	-cflags "-I../../../include"
+
+# Specify the top-level function for synthesis
+set_top		memcachedBuddy
+
+###########################
+# Solution settings
+
+# Create solution1
+open_solution -reset solution1
+
+set_part {xcvu095-ffva2104-2-e}
+create_clock -period 8 -name default
+config_rtl -encoding onehot -reset all -reset_level high -reset_async -vivado_impl_strategy default -vivado_phys_opt place -vivado_synth_design_args {-directive sdx_optimization_effort_high} -vivado_synth_strategy default
+set_clock_uncertainty 1.25
+
+# Simulate the C code
+# csim_design
+
+# Synthesis the C code
+csynth_design
+
+export_design -format ip_catalog -display_name "Combined Binary DRAM Memcached Pipeline with Buddy Allocator" -description "A 4 stage memcached pipeline with DRAM value stores supporting only the binary protocol" -vendor "xilinx.labs" -version "1.07"
+exit
---
+++ app/memcached/hls/sources/memcachedBuddy.cpp
@@ -0,0 +1,92 @@
+/************************************************
+Copyright (c) 2016, Xilinx, Inc.
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification,
+are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice,
+this list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice,
+this list of conditions and the following disclaimer in the documentation
+and/or other materials provided with the distribution.
+
+3. Neither the name of the copyright holder nor the names of its contributors
+may be used to endorse or promote products derived from this software
+without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.// Copyright (c) 2015 Xilinx, Inc.
+************************************************/
+#include "globals.h"
+
+void memcachedBuddy(stream<extendedAxiWord> &inData, stream<extendedAxiWord> &outData,
+					stream<memCtrlWord> &dramValueStoreMemRdCmd, stream<ap_uint<512> > &dramValueStoreMemRdData,
+					stream<memCtrlWord> &dramValueStoreMemWrCmd, stream<ap_uint<512> > &dramValueStoreMemWrData,
+					stream<ap_uint<512> > &hashTableMemRdData, stream<memCtrlWord> &hashTableMemRdCmd,
+					stream<ap_uint<512> > &hashTableMemWrData, stream<memCtrlWord> &hashTableMemWrCmd,
+					axis_buddy_alloc& alloc, axis_buddy_alloc_ret& alloc_ret,
+					ap_uint<1> &flushReq, ap_uint<1> flushAck, ap_uint<1> &flushDone) {
+
+	#pragma HLS INTERFACE ap_ctrl_none port=return
+	#pragma HLS INTERFACE ap_none register port=flushReq
+	#pragma HLS INTERFACE ap_none register port=flushAck
+	#pragma HLS INTERFACE ap_none register port=flushDone
+
+	#pragma HLS DATA_PACK 	variable=hashTableMemRdData
+	#pragma HLS DATA_PACK 	variable=hashTableMemRdCmd
+	#pragma HLS DATA_PACK 	variable=hashTableMemWrData
+	#pragma HLS DATA_PACK 	variable=hashTableMemWrCmd
+	#pragma HLS DATA_PACK 	variable=dramValueStoreMemRdCmd
+	#pragma HLS DATA_PACK 	variable=dramValueStoreMemRdData
+	#pragma HLS DATA_PACK 	variable=dramValueStoreMemWrCmd
+	#pragma HLS DATA_PACK 	variable=dramValueStoreMemWrData
+	#pragma HLS DATA_PACK 	variable=alloc
+	#pragma HLS DATA_PACK 	variable=alloc_ret
+
+	#pragma HLS INTERFACE port=inData axis
+	#pragma HLS INTERFACE port=outData axis
+	#pragma HLS INTERFACE port=hashTableMemWrData axis
+	#pragma HLS INTERFACE port=hashTableMemRdData axis
+	#pragma HLS INTERFACE port=hashTableMemRdCmd axis
+	#pragma HLS INTERFACE port=hashTableMemWrCmd axis
+	#pragma HLS INTERFACE port=dramValueStoreMemRdCmd axis
+	#pragma HLS INTERFACE port=dramValueStoreMemRdData axis
+	#pragma HLS INTERFACE port=dramValueStoreMemWrCmd axis
+	#pragma HLS INTERFACE port=dramValueStoreMemWrData axis
+
+	#pragma HLS INTERFACE port=alloc 			axis
+	#pragma HLS INTERFACE port=alloc_ret 		axis
+
+	#pragma HLS DATAFLOW interval=1
+
+	static stream<pipelineWord>	requestParser2hashTable("requestParser2hashTable");
+	static stream<pipelineWord>	hashTable2valueStoreDram("hashTable2valueStoreDram");
+	static stream<pipelineWord> valueStoreDram2responseFormatter("valueStoreDram2responseFormatter");
+
+	#pragma HLS DATA_PACK 	variable=requestParser2hashTable
+	#pragma HLS DATA_PACK 	variable=hashTable2valueStoreDram
+	#pragma HLS DATA_PACK 	variable=valueStoreDram2responseFormatter
+
+	#pragma HLS STREAM variable=requestParser2hashTable 			depth=16
+	#pragma HLS STREAM variable=hashTable2valueStoreDram			depth=16
+	#pragma HLS STREAM variable=valueStoreDram2responseFormatter	depth=16
+
+	binaryParser(inData, requestParser2hashTable);
+	hashTableWithBuddy(requestParser2hashTable, hashTable2valueStoreDram,
+					hashTableMemRdData, hashTableMemRdCmd,
+					hashTableMemWrData, hashTableMemWrCmd,
+					alloc, alloc_ret,
+					flushReq, flushAck, flushDone);
+	valueStoreDram(hashTable2valueStoreDram, dramValueStoreMemRdCmd, dramValueStoreMemRdData, dramValueStoreMemWrCmd, dramValueStoreMemWrData,
+					valueStoreDram2responseFormatter);
+	binaryResponse(valueStoreDram2responseFormatter, outData);
+}
