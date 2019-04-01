ee95e09d78c9a2321107aca006a9ecd62fb64830
Hythzz <24907190+hythzz@users.noreply.github.com>
2019-04-01 00:34:41 -0400

    add memcached with buddy top level script

--- app/memcached/hls/generate_hls.sh
+++ app/memcached/hls/generate_hls.sh
@@ -160,3 +160,39 @@ for ip in "${HLS_IP_CORES[@]}"; do
 	eval cp ${GENERATED_HLS_PROJECT}/solution1/impl/ip/${zipname} ${NEW_IP_REPO}/
 	unzip -o ${NEW_IP_REPO}/${zipname} -d ${NEW_IP_REPO}/${zipdir}
 done
+
+# Hardcoded through projects
+GENERATED_HLS_PROJECT="generated_mcd_buddy_project"
+HLS_DIR="$PWD"
+
+# Customize: new IP folder prefix
+PREFIX=app_mcd_core_pipeline_buddy
+
+# Check if the shared IP repo exists
+if [ ! -d "$GENERATED_IP_FOLDER" ]; then
+	mkdir "$GENERATED_IP_FOLDER"
+fi
+
+# Rolling for each HLS IP in this directory
+for ip in "${HLS_IP_CORES[@]}"; do
+	NEW_IP_REPO="${GENERATED_IP_FOLDER}/${PREFIX}_${ip}_${TARGET_BOARD}"
+	eval cd ${HLS_DIR}/${ip}
+
+	# Run the HLS script accoding to board
+	eval ${VIVADO_HLS} -f run_hls_${TARGET_BOARD}.tcl
+
+	if [ ! -d ${NEW_IP_REPO} ]; then
+		mkdir ${NEW_IP_REPO}
+	fi
+
+	zipname=`ls ${GENERATED_HLS_PROJECT}/solution1/impl/ip/*.zip`
+	zipname=$(basename ${zipname})
+	zipnamelen=${#zipname}
+
+	# Skip the suffix ".zip"
+	zipdir=${zipname:0:${zipnamelen}-4}
+
+	# Copy the IP archive into shared IP repo
+	eval cp ${GENERATED_HLS_PROJECT}/solution1/impl/ip/${zipname} ${NEW_IP_REPO}/
+	unzip -o ${NEW_IP_REPO}/${zipname} -d ${NEW_IP_REPO}/${zipdir}
+done
