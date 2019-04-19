ef2d78b44d89ba7d5791b708fd3a5ba3be7f6cd5
shinyehtsai <shinyehtsai@gmail.com>
2019-04-18 21:44:10 -0400

    [PATCH 4/6] Modify workloadStream to do RDMA access between two nodes

--- app/rdm_workload/workloadStream.c
+++ app/rdm_workload/workloadStream.c
@@ -1,26 +1,8 @@
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <stdio.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <netinet/tcp.h>
-#include <time.h>
-#include <getopt.h>
-#include <errno.h>
-#include <linux/types.h>
-#include <stdlib.h>
-
-#include <arpa/inet.h>
-#include <linux/if_packet.h>
-#include <sys/ioctl.h>
-#include <sys/socket.h>
-#include <net/if.h>
-#include <netinet/ether.h>
-
+#include "workloadStream.h"
 enum INTERFACE_TYPE {
     INTERFACE_ETHERNET = 1,
-    INTERFACE_PCIE = 2
+    INTERFACE_PCIE = 2,
+    INTERFACE_RDMA = 3
 };
 #define DEFAULT_REPLY_SIZE 4096
 
@@ -28,6 +10,8 @@ enum INTERFACE_TYPE {
 
 uint64_t MAX_REQUEST = 0xffffffffffffffff;
 uint64_t IGNORE_REQUEST = 0;
+uint64_t MAX_ACCESS_OFFSET = 0;
+int MACHINE_ID = -1;
 
 struct osdi_mem_workload_struct {
     // uint32_t batch_id;
@@ -47,10 +31,6 @@ struct osdi_mem_workload_header {
     uint32_t size;  // should be multiple of 4K pages or DEFAULT_PAGE_SIZE
 };
 
-/* OSDI '16 TRACE */
-// please download the trace from https://github.com/NetSys/disaggregation
-// this program use mem-trace (use graphlab as an example since graphlab trace is smaller)
-
 typedef struct osdi_mem_workload_struct workload_struct;
 typedef struct osdi_mem_workload_header workload_header;
 
@@ -64,7 +44,8 @@ static void printHelp(char *name) {
         "\t-w \tworkload data. "
         "(graphlab/0-mem-ec2-54-197-111-1.compute-1.amazonaws.com.merged)\n");
     printf("\t-n \tmax request num. \n");
-    printf("\t-i \tignore first I lines. \n");
+    printf("\t-i \tignore first i lines. \n");
+    printf("\t-I \tmachine id. \n");
     printf("\t-p \tpad_size. pad Bytes before payload\n");
     printf("\t-m \tmode. eth/pcie\n");
     printf("\t-v \tverbose mode\n");
@@ -73,6 +54,10 @@ static void printHelp(char *name) {
         "\t./workloadStream -w "
         "graphlab/0-mem-ec2-54-197-111-1.compute-1.amazonaws.com.merged -p 14 "
         "-m pcie -n 1000 -i 50000 -v\n");
+    printf(
+        "./workloadStream.o -w "
+        "graphlab/0-mem-ec2-54-197-111-1.compute-1.amazonaws.com.merged -p 14 "
+        "-m rdma -n 1000 -i 50000 -v -I 1\n");
     printf("\n");
 }
 
@@ -98,7 +83,7 @@ void myDump(char *desc, uint8_t *addr, int len) {
         }
         printf(" %02x", pc[i]);
     }
-    printf("\n");
+    printf("\n %d", fast_rand(5));
 }
 
 int getWorkload(size_t **size_array_ptr, uint8_t ***packet_array_ptr,
@@ -167,6 +152,12 @@ int getWorkload(size_t **size_array_ptr, uint8_t ***packet_array_ptr,
         workload_array[current_request].time_stamp = _time_stamp;
         workload_array[current_request].size =
             _request_size * DEFAULT_PAGE_SIZE;
+        if (workload_array[current_request].size +
+                workload_array[current_request].offset >
+            MAX_ACCESS_OFFSET) {
+            MAX_ACCESS_OFFSET = workload_array[current_request].size +
+                                workload_array[current_request].offset;
+        }
         if (_last_time == 0)
             _last_time = workload_array[current_request].time_stamp;
         if (current_request > 0)
@@ -207,6 +198,99 @@ int getWorkload(size_t **size_array_ptr, uint8_t ***packet_array_ptr,
     *interarrival_array_ptr = interarrival_array;
 
     return num_of_reqs;
+}
+
+int deliverRequestRDMA(int request_num, size_t *size_array,
+                       uint8_t **packet_array, uint32_t *interarrival_array,
+                       int pad_size) {
+    struct ib_inf *node_inf;
+    node_inf = ib_setup(1, MACHINE_ID);
+    struct ibv_mr *tmp_mr;
+    struct ib_mr_attr mr_attr;
+    void *check_space = malloc(MAX_ACCESS_OFFSET);
+    char mem_mr_name[RDMA_MAX_QP_NAME];
+    sprintf(mem_mr_name, "test_mr_key");
+    tmp_mr = ibv_reg_mr(node_inf->pd, check_space, MAX_ACCESS_OFFSET,
+                        IBV_ACCESS_LOCAL_WRITE | IBV_ACCESS_REMOTE_WRITE |
+                            IBV_ACCESS_REMOTE_READ);
+    mr_attr.addr = (uint64_t)tmp_mr->addr;
+    mr_attr.rkey = tmp_mr->rkey;
+
+    if (MACHINE_ID == 1) {
+        memcached_publish(mem_mr_name, &mr_attr, sizeof(struct ib_mr_attr));
+        printf("publish %s %llu %llu\n", mem_mr_name,
+               (unsigned long long)mr_attr.addr,
+               (unsigned long long)mr_attr.rkey);
+        /*while(1)
+        {
+            printf("%s\n", check_space);
+            sleep(1);
+        }*/
+        while (1)
+            ;
+    } else {
+        stick_this_thread_to_core(2);
+        struct ib_mr_attr *get_mr;
+        int ret_len;
+        do {
+            ret_len = memcached_get_published(mem_mr_name, (void **)&get_mr);
+        } while (ret_len <= 0);
+
+        int each_request;
+
+        struct timespec start, end;
+        uint64_t page_num;
+        double *time_diff, average_sum, per_page_sum;
+
+        /* Send packet */
+        time_diff = malloc(sizeof(double) * request_num);
+        for (each_request = 0; each_request < request_num; each_request++) {
+            // copy header into send_addr
+            // issue request
+            workload_header *hptr =
+                (void *)packet_array[each_request] + pad_size;
+            /*printf("%d:\t %lld %lld %lld %c\n", each_request,
+                   (long long int)interarrival_array[each_request],
+                   (long long int)hptr->offset, (long long int)hptr->size,
+                   hptr->mode);*/
+            clock_gettime(CLOCK_MONOTONIC, &start);
+            // if it's a read request
+            if (hptr->mode == 'r') {
+                userspace_one_read(node_inf->conn_qp[0], tmp_mr, hptr->size,
+                                   get_mr, hptr->offset);
+                userspace_one_poll(node_inf->conn_cq[0], 1);
+            } else if (hptr->mode == 'w') {  // if it's a write request
+                userspace_one_write(node_inf->conn_qp[0], tmp_mr, hptr->size,
+                                    get_mr, hptr->offset);
+                userspace_one_poll(node_inf->conn_cq[0], 1);
+            }
+
+            // this part should be done once LEGOFPGA has interface
+            clock_gettime(CLOCK_MONOTONIC, &end);
+            time_diff[each_request] = diff_ns(&start, &end);
+
+            // I found out that using usleep will slow the overall latency - I
+            // am not sure why
+            // since this is a latency test, I manually disable interarrival
+            // sleep
+            // usleep(interarrival_array[each_request]);
+        }
+
+        average_sum = 0;
+        per_page_sum = 0;
+        for (each_request = 0; each_request < request_num; each_request++) {
+            page_num = (size_array[each_request] - pad_size -
+                        sizeof(workload_header)) /
+                       DEFAULT_PAGE_SIZE;
+            average_sum += time_diff[each_request];
+            per_page_sum += time_diff[each_request] / page_num;
+        }
+
+        printf("average latency: %f (ns)\n", average_sum / request_num);
+        printf("per_page(per_request per_page) latency: %f (ns)\n",
+               per_page_sum / request_num);
+    }
+    return 0;
 }
 
 int deliverRequestPCIe(int request_num, size_t *size_array,
@@ -343,6 +427,10 @@ int deliverRequest(int request_num, size_t *size_array, uint8_t **packet_array,
                                      interarrival_array, pad_size,
                                      pcie_send_addr, pcie_recv_addr);
             break;
+        case INTERFACE_RDMA:
+            ret = deliverRequestRDMA(request_num, size_array, packet_array,
+                                     interarrival_array, pad_size);
+            break;
         default:
             printf("interface %d is not implemented yet\n", interface);
             exit(1);
@@ -365,6 +453,7 @@ int main(int argc, char *argv[]) {
         {"workload_file_name", required_argument, 0, 'w'},
         {"max request", required_argument, 0, 'n'},
         {"ignore request", required_argument, 0, 'i'},
+        {"machine id", required_argument, 0, 'I'},
         {"mode (pcie/eth)", required_argument, 0, 'm'},
         {"pad_size", required_argument, 0, 'p'},
         {"mode", required_argument, 0, 'p'},
@@ -372,7 +461,7 @@ int main(int argc, char *argv[]) {
         {"help", no_argument, 0, 'h'},
         {NULL, 0, 0, 0}};
 
-    while ((opt = getopt_long(argc, argv, "vhw:p:m:n:i:", long_options,
+    while ((opt = getopt_long(argc, argv, "vhw:p:m:n:i:I:", long_options,
                               &option_index)) >= 0) {
         switch (opt) {
             case 0:
@@ -385,6 +474,9 @@ int main(int argc, char *argv[]) {
                 break;
             case 'i':
                 IGNORE_REQUEST = atoi(optarg);
+                break;
+            case 'I':
+                MACHINE_ID = atoi(optarg);
                 break;
             case 'm':
                 deliver_mode = optarg;
@@ -420,6 +512,8 @@ int main(int argc, char *argv[]) {
     // parse workload
     request_num = getWorkload(&size_array, &packet_array, &interarrival_array,
                               workload_file_name, pad_size);
+    printf("request num:%d max_offset:%llu\n", request_num,
+           (unsigned long long)MAX_ACCESS_OFFSET);
 
     // deliver request
     if (strcmp(deliver_mode, "eth") == 0)
@@ -428,6 +522,9 @@ int main(int argc, char *argv[]) {
     else if (strcmp(deliver_mode, "pcie") == 0)
         deliverRequest(request_num, size_array, packet_array,
                        interarrival_array, pad_size, INTERFACE_PCIE);
+    else if (strcmp(deliver_mode, "rdma") == 0)
+        deliverRequest(request_num, size_array, packet_array,
+                       interarrival_array, pad_size, INTERFACE_RDMA);
     else {
         printf("error mode %s\n", deliver_mode);
         printHelp(deliver_mode);
